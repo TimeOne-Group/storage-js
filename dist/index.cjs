9edebcb0caa5e333a889bcea896dccc1863bc907
Fabien Barbaud <fabien.barbaud@timeonegroup.com>
2021-03-07 16:52:14 +0100

    fix: change main path

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,5 +1,5 @@
 
-/*! @timeone-group/storage-js 0.1.2 https://github.com/https://github.com/TimeOne-Group/storage-js#readme @license GPL-3.0 */
+/*! @timeone-group/storage-js 0.1.3 https://github.com/https://github.com/TimeOne-Group/storage-js#readme @license GPL-3.0 */
 'use strict';
 
 var ceil = Math.ceil;
